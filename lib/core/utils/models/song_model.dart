225e97183a4b4278abbfd9445a5fd96abaa5a8ae
MarioYoussef <marioyoussef08@gmail.com>
2025-08-30 19:35:12 +0300

    create song model

---
+++ lib/core/utils/models/song_model.dart
@@ -0,0 +1,17 @@
+class SongModel {
+  final String title;
+  final String artist;
+  final String album;
+  final String path;
+  final String duration;
+  final String image;
+
+  SongModel({
+    required this.title,
+    required this.artist,
+    required this.album,
+    required this.path,
+    required this.duration,
+    required this.image,
+  });
+}
